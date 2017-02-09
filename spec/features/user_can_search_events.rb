59faf855e5fedd617e0125de55dd805cbac4d44a
Caroline Powell <carpowell10@gmail.com>
2017-02-09 09:15:40 -0700

    Add user story to spec/features

---
+++ spec/features/user_can_search_events.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+
+
+# As a user,
+# When I fill in the search with 80202,
+# And I click Find Events,
+# The current path is `/search`,
+# And I can see a list of 16 events with a header `16 events for March', sorted by popularity.
+# For each event I can see the Event name, date & time, description (shortened to 140 characters), venue name (url to the venue page), venue address.
