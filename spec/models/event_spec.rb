ad1fabb724406c8fafa3aa4e38282812dccc76ee
David Knott <david1k1nott@gmail.com>
2017-02-09 10:30:27 -0700

    create and pass event model test

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Event do
+  it "can search events by zip" do
+    VCR.use_cassette("eventful_find_by_zip") do
+      events = Event.search_by("80202")
+
+      expect(events.count).to eq 16
+      expect(events.first.name).to eq 'Experience Hendrix'
+      expect(events.last.venue).to eq 'Summit Music Hall'
+    end
+  end
+end
