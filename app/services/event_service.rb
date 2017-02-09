8c868b579ae2685767488018a8e8fb00c05dab85
Caroline Powell <carpowell10@gmail.com>
2017-02-09 10:42:14 -0700

    Add correct api call

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -3,7 +3,7 @@ class EventService
     # link to docs http://api.eventful.com/docs/events/search
     # refactor the faraday.get and params
     # I think the March piece should really be Next+Month, get it working then try it out
-    response = Faraday.get("http://api.eventful.com/rest/events/search?l=#{zipcode}")
+    response = Faraday.get("http://api.eventful.com/rest/events/search?l=#{zipcode}&app_key=#{ENV['APP_KEY']}")
     binding.pry
     JSON.parse(response.body, symbolize_names: true)[:events]
 
