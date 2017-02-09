403bea2885fabbc97c34965bbccdf0be50e2295d
Caroline Powell <carpowell10@gmail.com>
2017-02-09 11:00:46 -0700

    Work on extracting event data

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,12 +8,15 @@ class Event
               :venue_address
 
   def initialize(raw_event)
-    @name = raw_event[:name]
-    @datetime = raw_event[:tbd]
-    @description = raw_event[:tbd]
-    @venue_name = raw_event[:tbd]
-    @venue_url = raw_event[:tbd]
-    @venue_address = raw_event[:tbd]
+    @name = raw_event["title"]
+    @datetime = raw_event["start_time"]
+    @description = raw_event["description"]
+    @venue_name = raw_event["venue_name"]
+    @venue_url = raw_event["venue_url"]
+    @street_address = raw_event["venue_address"]
+    @city = raw_event["city_name"]
+    @state = raw_event["region_name"]
+    @zipcode = raw_event["postal_code"]
   end
 
   def self.find_by_zip(zipcode)
@@ -25,5 +28,9 @@ class Event
   def shorten_description(description, max_length = 140)
     return description.slice(0, max_length) if description.length > max_length
     description
+  end
+
+  def venue_address
+    "#{@street_address} #{@city_name}, #{@state} #{@zipcode}"
   end
 end
--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -3,9 +3,9 @@ class EventService
     # link to docs http://api.eventful.com/docs/events/search
     # refactor the faraday.get and params
     # I think the March piece should really be Next+Month, get it working then try it out
-    response = Faraday.get("http://api.eventful.com/rest/events/search?l=#{zipcode}&app_key=#{ENV['APP_KEY']}")
-    binding.pry
-    JSON.parse(response.body, symbolize_names: true)[:events]
-
+    response = Faraday.get("http://api.eventful.com/rest/events/search?l=#{zipcode}&t=March+2017&app_key=#{ENV['APP_KEY']}")
+    hash = Hash.from_xml(response.body.gsub("\n", ""))
+    symbolized_hash = hash.symbolize_keys
+    events = symbolized_hash[:search]["events"]["event"]
   end
 end
