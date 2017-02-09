40dc6ef66619616dbc280c03f3929b5919b7a6c9
Daniel Rodriguez <drod2793@gmail.com>
2017-02-09 09:35:40 -0700

    First attempt at getting response

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
     zip_code = params[:q]
+    response = Faraday.get "http://api.eventful.com/rest/events/search?api_key=8gWGVrJnPdvtqFrq&location=#{zip_code}"
+    binding.pry
   end
 end
