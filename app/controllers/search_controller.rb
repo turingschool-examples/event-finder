9318a63395ac8b33eaf5d644353834511562f5b2
Daniel Rodriguez <drod2793@gmail.com>
2017-02-09 09:58:25 -0700

    Finally parsing JSON correctly

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,7 @@
 class SearchController < ApplicationController
   def index
     zip_code = params[:q]
-    response = Faraday.get "http://api.eventful.com/rest/events/search?app_key=8gWGVrJnPdvtqFrq&location=#{zip_code}&sort_order=popularity"
-    # parsed = JSON.parse(response.body)
-    binding.pry
+    response = Faraday.get "http://api.eventful.com/json/events/search?app_key=8gWGVrJnPdvtqFrq&location=#{zip_code}&sort_order=popularity"
+    parsed = JSON.parse(response.body, symbolize_names: true)
   end
 end
