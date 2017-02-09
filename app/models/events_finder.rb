159034f392fdc836b88de1bb45f43a778e229a34
Molly Brown <mollbrown@gmail.com>
2017-02-09 10:21:27 -0700

    add attributes to events finder

--- app/models/events_finder.rb
+++ app/models/events_finder.rb
@@ -1,7 +1,16 @@
 class EventsFinder
 
+  attr_reader: :title, :start_time, :description, :veune_name, :venue_address, :venue_url
+
   def initialize(attributes = {})
-    @total_events = [:total_items]
+    # Event name, date & time, description (shortened to 140 characters), venue name (url to the venue page), venue address.
+    @event_name = attributes[:title]
+    @event_date_time = attributes[:start_time]
+    @event_description = attributes[:description]
+    @event_venue_name = attributes[:venue_name]
+    @event_venue_address = attributes[:venue_address]
+    @event_venue_url = attributes[:venue_url]
+    # @total_events = [:events][:event].count
   end
 
   def self.find_by_zip(zip)
--- app/services/events_service.rb
+++ app/services/events_service.rb
@@ -2,9 +2,6 @@ class EventsService
 
   def self.find_by_zip(zip)
     response = Faraday.get("http://api.eventful.com/json/events/search?location=#{zip}&app_key=g52t5VnTpTfh2pZM")
-    JSON.parse(response.body, symbolize_names:true)
-    byebug
+    JSON.parse(response.body, symbolize_names:true)[:events][:event]
   end
 end
-
-# keys [:last_item, :total_items, :first_item, :page_number, :page_size, :page_items, :search_time, :page_count, :events]
