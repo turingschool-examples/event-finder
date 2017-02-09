f1bb7b2e52456bc492ed1fb8764d67e65dba2b6a
Ethan Bennett <ebennett19@gmail.com>
2017-02-09 10:09:16 -0700

    I have all my data in JSON, experimenting with how to access the information I need

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://api.eventful.com/rest/events/search?app_key=MT9FKLGpN52KX9cX&location=80202&sort_order=popularity&page_size=16")
+    response = Faraday.get("https://api.eventful.com/json/events/search?app_key=#{ENV['EVENTFUL_KEY']}&location=80202&sort_order=popularity&page_size=16")
+    data = JSON.parse(response.body, symbolize_names: true)
     binding.pry
   end
 end
