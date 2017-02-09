3e96dec917141022bc772a6337ddd4ffde77202a
Daniel Rodriguez <drod2793@gmail.com>
2017-02-09 10:38:18 -0700

    Extracts service class

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
     zip_code = params[:q]
-    response = Faraday.get "http://api.eventful.com/json/events/search?app_key=8gWGVrJnPdvtqFrq&location=#{zip_code}&sort_order=popularity&page_size=16"
-    parsed = JSON.parse(response.body, symbolize_names: true)[:events][:event]
+    parsed = Service.get_events(zip_code)
+
 
     @events = Event.get_events(parsed)
   end
