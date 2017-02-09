47184db3b8335e06f62364475dba823c8120f606
Jesse Shipley <jesse.radack@gmail.com>
2017-02-09 09:35:33 -0700

    add database cleaner

--- spec/features/user_can_see_search_results_spec.rb
+++ spec/features/user_can_see_search_results_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
-# As a user, 
-# When I fill in the search with 80202, 
-# And I click Find Events, 
-# The current path is `/search`,
+
+describe "User" do
+  it "can search for events by zipcode" do
+    # As a user, 
+
+    # When I fill in the search with 80202, 
+    # And I click Find Events, 
+    # The current path is `/search`,
+    
+  end
+end
 # And I can see a list of 16 events with a header `16 events for March', sorted by popularity. 
 # For each event I can see the Event name, 
 # date & time, 
