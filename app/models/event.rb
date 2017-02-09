a17f561fa50eb6de1f9a2d66765ccdfedd2a8414
Jesse Shipley <jesse.radack@gmail.com>
2017-02-09 11:01:03 -0700

    finally parsed xml to create event objects

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,13 +6,19 @@ class Event
               :venue_address
 
   def initialize(attrs = {})
-
+    @name = attrs[]
+    @name = attrs[]
+    @name = attrs[]
+    @name = attrs[]
+    @name = attrs[]
+    byebug
   end
 
   def self.list(params)
     response = EventService.new.gather_events(params)
-    byebug
-    .each do |event|
+    results = Hash.from_trusted_xml(response.body)
+    
+    results["search"]["events"]["event"].each do |event|
       new(event)
     end
   end
