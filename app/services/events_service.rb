bdb20ec13aa0f8353704d16a6234e16adb0da832
Jason Conrad <jdconrad@liberty.edu>
2017-02-09 10:21:14 -0700

    fifth commit: api consumed working on pulling needed information

--- app/services/events_service.rb
+++ app/services/events_service.rb
@@ -1,9 +1,9 @@
 class EventsService
 
   def self.find_by_zipcode(zipcode)
-    response = Faraday.get("http://eventful.com/events?q=music&l=#{zipcode}/client_id=#{ENV["CLIENT_ID"]}")
-    byebug
-    events = JSON.parse(response.bodt)
+    response = Faraday.get("http://eventful.com/app_key=#{ENV["CLIENT_ID"]}events/search?...location=#{zipcode}.json")
+    events = JSON.parse(response.body)
+    by3ebug
 
   end
 end
