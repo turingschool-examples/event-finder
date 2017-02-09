ff6d553278f9caeb341e9622813400a870b63cee
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-09 10:06:08 -0700

    Update API call to limit to one page 16 results. Add ugly code into search_controller to parse API response

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
   def index
-    raw_response = Faraday.get 'http://api.eventful.com/json/events/search?app_key=7J7CGGFM5zgs8RH7&location=80202&date=2017030100-2017033100&sort_order=popularity'
+    @events = []
+    raw_response = Faraday.get 'http://api.eventful.com/json/events/search?app_key=7J7CGGFM5zgs8RH7&location=80202&date=2017030100-2017033100&sort_order=popularity&page_size=16&page_number=1'
     response = JSON.parse(raw_response.body)
-    byebug
+    requested_events = response["events"]["event"]
+    requested_events.each do |one_event|
+      @events << Event.new(one_event)
+    end
     # @events = 
   end
 end
