99fe840436ef7f8b1e7407505f43f6cda82ef13d
Ann Wolff <annwolffe@gmail.com>
2017-02-09 11:04:45 -0700

    Refactor filter attributes

--- app/models/results_presenter.rb
+++ app/models/results_presenter.rb
@@ -2,11 +2,18 @@ class ResultsPresenter
   attr_reader :events
   
   def initialize(raw_events)
-    @events = filter_attributes(raw_events)
+    @events = filter_attributes(raw_events['events']['event'])
   end
 
   def filter_attributes(raw_events)
+    raw_events.map do |event|
+      {'name' = event['title'],
+      'date' = event['start_time'],
+      'description' = event['description']
+      'venue_name' = event['venue_name'],
+      'venue_url' = event['venue_url'],
+      'venue_address' = event['venue_address']}
+    end
     byebug
-    
   end
 end
