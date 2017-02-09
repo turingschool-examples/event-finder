d4a1fd5135adbb2675a52fdb139b9b79a8fd28dd
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-09 08:59:17 -0700

    Add factory girl rb file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,7 @@ VCR.configure do |config|
 end
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
