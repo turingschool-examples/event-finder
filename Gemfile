82703c3447d410019232c7f12207f7c7f783d835
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-09 10:04:51 -0700

    Add environment variable for app_key

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'httparty'
 gem 'faraday'
+gem 'figaro'
 
 group :development, :test do
   gem 'byebug'
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get()
+    zipcode = params[:q]
+    byebug
+    response = Faraday.get("http://api.eventful.com/json/events/search?location=#{zipcode}&total_items=16&date=March&app_key=#{ENV['EVENTFUL_API_KEY']}")
+    @events = response.body
   end
 end
