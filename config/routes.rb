ab1620d3913e1b82d1ace689d7be56864db6da4a
Amz K <akintner@u.rochester.edu>
2017-02-09 09:21:05 -0700

    add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/search', to: "search#index"
 
-  post 'http://eventful.com/oauth/request_token', to: "sessions#create"
+  post 'http://eventful.com/oauth/request_token', to: "sessions#new"
   get 'http://eventful.com/oauth/authorize', as: :event_login
   post 'http://eventful.com/oauth/access_token', to: "sessions#create"
 end
