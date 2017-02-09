bbbd1eeaf2170c32d5c6204f52f6f8bddd8b49e9
Brett Rivers <riverswb@gmail.com>
2017-02-09 10:39:00 -0700

    APIs are easier to use with the correct key...

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,13 @@ class SearchController < ApplicationController
   def index
     zip = params[:q]
     date = "March"
-    conn = Faraday.new(url: "http://api.eventful.com/rest/events/search")
-    response = conn.post do |p|
-      p.headers['app_key'] = "BKCFLWH22GnWSjtMs"
-      p.params[:location] = 80202
+    conn = Faraday.new(url: "http://api.eventful.com/json/events/search")
+    response = conn.get do |p|
+      p.params[:app_key] = "BKCFLWH22GnWSjtM"
+      p.params[:location] = zip
+      p.params[:date] = date
     end
-    # faraday[:headers] = :app_key => "BKCFLWH22GnWSjtMs"
-    # response = conn.post #, {:app_key => "BKCFLWH22GnWSjtMs"}
 
-    # key = BKCFLWH22GnWSjtMs
 
 
   end
