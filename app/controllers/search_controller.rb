7d30595c7b72c11485668cb1c896b3546cd1695e
Brett Rivers <riverswb@gmail.com>
2017-02-09 09:51:42 -0700

    Setting up faraday

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
+    zip = params[:q]
+    date = "March"
+    conn = Faraday.post("http://api.eventful.com/rest/events/search?BKCFLWH22GnWSjtMs&location=zip&") do |faraday|
+      faraday.get
+    end
+
 
   end
 end
