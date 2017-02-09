c9897cf07c006bad07387a36c35aefebfbc8a05a
Jason Conrad <jdconrad@liberty.edu>
2017-02-09 10:52:47 -0700

    seventh commit:

--- app/services/events_service.rb
+++ app/services/events_service.rb
@@ -1,7 +1,7 @@
 class EventsService
 
   def self.find_by_zipcode(zipcode)
-    response = Faraday.get("http://eventful.com/app_key=#{ENV["CLIENT_ID"]}/json/events?q=music&l=#{zipcode}")
+    response = Faraday.get("http://api.eventful.com/json/app_key=#{ENV["CLIENT_ID"]}/events/search?...&location=#{zipcode}")
       byebug
   end
 end
