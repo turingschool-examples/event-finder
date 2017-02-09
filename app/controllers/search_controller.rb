c309f90735450cffbb42fdce50875445eb7bb0ec
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-09 09:49:04 -0700

    Add VCR for spec file, add Faraday call to search_controller index, add table row count to spec file

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    response = Faraday.get 'http://api.eventful.com/rest/events/search?app_key=7J7CGGFM5zgs8RH7&location=80202&date=2017030100-2017033100&sort_order=popularity'
+    byebug
+    # @events = 
   end
 end
--- spec/features/search_events_by_location_spec.rb
+++ spec/features/search_events_by_location_spec.rb
@@ -2,25 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Event Search by location' do
   scenario 'When zip code entered into search box' do
-    # As a user, 
-    visit root_path
+    VCR.use_cassette("search_by_location") do
+      # As a user, 
+      visit root_path
 
-    # When I fill in the search with 80202, 
-    within('.navbar') do
-      fill_in 'q', with: '8202'
+      # When I fill in the search with 80202, 
+      within('.navbar') do
+        fill_in 'q', with: '8202'
+      end
+      # And I click Find Events, 
+      click_on 'Find Events'
+
+      # The current path is `/search`,
+      expect(current_path).to eq(search_path)
+      # And I can see a list of 16 events with a header `16 events for March', sorted by popularity. 
+      expect(page).to have_content('Event count: 16')
+      # For each event I can see the Event name, date & time, description (shortened to 140 characters), venue name (url to the venue page), venue address.
+      expect(page).to have_content('Event name')
+      expect(page).to have_content('Date & Time')
+      expect(page).to have_content('Description')
+      expect(page).to have_content('Venue name')
+      expect(page).to have_content('Venue address')
+      within("table") do
+        expect(all("tr").count).to eq(17)
+      end
     end
-    # And I click Find Events, 
-    click_on 'Find Events'
-
-    # The current path is `/search`,
-    expect(current_path).to eq(search_path)
-    # And I can see a list of 16 events with a header `16 events for March', sorted by popularity. 
-    expect(page).to have_content('Event count: 16')
-    # For each event I can see the Event name, date & time, description (shortened to 140 characters), venue name (url to the venue page), venue address.
-    expect(page).to have_content('Event name')
-    expect(page).to have_content('Date & Time')
-    expect(page).to have_content('Description')
-    expect(page).to have_content('Venue name')
-    expect(page).to have_content('Venue address')
   end
 end
