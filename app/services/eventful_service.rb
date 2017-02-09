c4e7f2f54e3f74aeb801ca63ada2c9b07d286e53
David Knott <david1k1nott@gmail.com>
2017-02-09 10:37:16 -0700

    add eventful app key to application.yml

--- app/services/eventful_service.rb
+++ app/services/eventful_service.rb
@@ -1,6 +1,6 @@
 class EventfulService
   def self.search_by(zip)
-    raw_response = Faraday.get "http://api.eventful.com/json/events/search?where=#{zip}&app_key=Q5gxTfWsVGgbDX8r&when=march&sort_order=popularity&page_size=16"
+    raw_response = Faraday.get "http://api.eventful.com/json/events/search?where=#{zip}&app_key=#{ENV["eventful_app_key"]}&when=march&sort_order=popularity&page_size=16"
     response = JSON.parse(raw_response.body, symbolize_names: true)[:events][:event]
   end
 end
