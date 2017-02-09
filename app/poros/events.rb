8b94bc4abff97cf5d588888db06b8151a16a7066
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-09 10:52:31 -0700

    Formatting description

--- app/poros/events.rb
+++ app/poros/events.rb
@@ -3,12 +3,12 @@ class Events
   attr_reader :title, :date_time, :description, :venue, :venue_address, :url
 
   def initialize(attributes={})
-    @title = attributes['title']
-    @date_time = attributes['start_time'] 
-    @description = attributes['description']
-    @venue = attributes['venue_name']
+    @title         = attributes['title']
+    @date_time     = attributes['start_time'] 
+    @description   = formatted_description(attributes['description'])
+    @venue         = attributes['venue_name']
     @venue_address = attributes['venue_address']
-    @url = attributes['venue_url']
+    @url           = attributes['venue_url']
   end
 
 
@@ -17,6 +17,14 @@ class Events
       event[1].map do |eve|
         new(eve)
       end
+    end
+  end
+
+  def formatted_description(desc)
+    if desc.count > 140
+      return desc
+    else
+      return desc
     end
   end
 
