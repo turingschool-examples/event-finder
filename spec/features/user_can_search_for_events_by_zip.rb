2a0a1343eb12b9e95007b4c8b45e5bb7ed10c4ab
David Knott <david1k1nott@gmail.com>
2017-02-09 09:34:11 -0700

    create user can find by zip feature test

--- spec/features/user_can_search_for_events_by_zip.rb
+++ spec/features/user_can_search_for_events_by_zip.rb
@@ -1,3 +1,29 @@
 require 'rails_helper'
 
 describe "When a user searchs for an event"
+  scenario "they can find an event by zip code"
+    visit root_path
+
+    fill_in :q, :with => "80203"
+
+    click_on "Find Events"
+
+    expect(current_path).to eq '/search'
+    expect(page).to have_content "16 events for March"
+    expect(page).to have_content "Experience Hendrix"
+    expect(page).to have_content "2017-03-07 19:30:00"
+    expect(page).to have_content "Paramount Theatre"
+    expect(page).to have_link "http://denver.eventful.com/venues/paramount-theatre-/V0-001-000198697-3?utm_source=apis&amp;utm_medium=apim&amp;utm_campaign=apic"
+    expect(page).to have_content "1621 Glenarm Place"
+    expect(page).to have_content "Mile High Wine Tours is the best way to enjoy Denver"
+  end
+end
+
+
+
+
+
+
+
+  end
+end
