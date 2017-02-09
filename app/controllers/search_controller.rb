0ddddae97be2c07ff277206fbf0321a6b63d4f95
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-09 10:40:37 -0700

    Change to double quotes in Faraday call, to interpolate params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
 class SearchController < ApplicationController
   def index
     @events = []
-    byebug
     # raw_response = Faraday.get 'http://api.eventful.com/json/events/search?app_key=7J7CGGFM5zgs8RH7&location=80202&date=2017030100-2017033100&sort_order=popularity&page_size=16&page_number=1'
-    raw_response = Faraday.get 'http://api.eventful.com/json/events/search?app_key=7J7CGGFM5zgs8RH7&location=#{params[:q]}&date=2017030100-2017033100&sort_order=popularity&page_size=16&page_number=1'
+    raw_response = Faraday.get "http://api.eventful.com/json/events/search?app_key=7J7CGGFM5zgs8RH7&location=#{params[:q]}&date=2017030100-2017033100&sort_order=popularity&page_size=16&page_number=1"
     response = JSON.parse(raw_response.body)
     requested_events = response["events"]["event"]
     requested_events.each do |one_event|
