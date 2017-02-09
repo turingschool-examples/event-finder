a8c87fdb3f11b8ab9c3d1bb9398b9ba06748493d
Ethan Bennett <ebennett19@gmail.com>
2017-02-09 09:56:44 -0700

    I'm getting the data I want now, but I need to change the response to JSON, test, and extract to service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://api.eventful.com/events?app_key=#{ENV['EVENTFUL_KEY']}&l=80202&sort_order=popularity&page_size=16")
+    response = Faraday.get("https://api.eventful.com/rest/events/search?app_key=MT9FKLGpN52KX9cX&location=80202&sort_order=popularity&page_size=16")
     binding.pry
   end
 end
