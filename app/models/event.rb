ac491bcb19f4760a18416e5fe4507b4077432631
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-09 10:19:23 -0700

    Add event detail methods to event model, to be able to siplay information in the view

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,34 @@
 class Event
   def initialize(raw_event_details)
-    byebug
     @_name = raw_event_details["title"]
     @_date_time = raw_event_details["start_time"]
     @_description = raw_event_details["description"]
     @_venue_name = raw_event_details["venue_name"]
     @_venue_url = raw_event_details["venue_url"]
     @_venue_address = raw_event_details["venue_address"]
+  end
+
+  def name
+    @_name
+  end
+
+  def date_time
+    @_date_time
+  end
+
+  def description
+    @_description
+  end
+
+  def venue_name
+    @_venue_name
+  end
+
+  def venue_address
+    @_venue_address
+  end
+
+  def venue_url
+    @_venue_url
   end
 end
