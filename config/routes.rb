f17ae9a381766d5d5f63c527fe89052b59ba39b5
Ethan Bennett <ebennett19@gmail.com>
2017-02-09 09:36:51 -0700

    Try to add oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/search', to: "search#index"
+  get '/auth/eventful', as: :eventful_login
+  get '/auth/eventful/callback', to: "sessions#create"
 end
