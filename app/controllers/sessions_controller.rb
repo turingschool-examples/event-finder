41e338b413f614b4378e445e3100a47f590ef80a
Amz K <akintner@u.rochester.edu>
2017-02-09 10:40:15 -0700

    still trying to parse all this data correctly...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,6 @@
 class SessionsController < ApplicationController
 
   def create
-    # @response = Faraday.post('http://eventful.com/oauth/request_token')
-    # token = @response.body.split(/\W+/)[1]
-    # user_data = Faraday.get("https://api.github.com/user?access_token=#{token}")
-    # auth = JSON.parse(user_data.body)
-
-
-    # redirect_to dashboard_index_path
+  
   end
 end
--- app/models/event_finder.rb
+++ app/models/event_finder.rb
@@ -1,5 +1,14 @@
 class EventFinder
   def initialize(attributes ={})
+    @attrs = attributes
+    # @title = attributes[:title]
+    # @venue = attributes[:venue_name]
+    # @city = attributes[:city_name]
+    # @state = attributes[:region_name]
+  end
+
+  def attendees
+    @attrs[:going] + @attrs[:going_count] = @attrs[:watching_count]
   end
 
   def self.closest_to_zip(zip)
