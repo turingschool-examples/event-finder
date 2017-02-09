9240088f1672375a3d2df75a53d873ca550ddde6
Daniel Rodriguez <drod2793@gmail.com>
2017-02-09 10:20:51 -0700

    Adds event class and get events class method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,26 @@
 class SearchController < ApplicationController
   def index
     zip_code = params[:q]
-    response = Faraday.get "http://api.eventful.com/json/events/search?app_key=8gWGVrJnPdvtqFrq&location=#{zip_code}&sort_order=popularity"
-    parsed = JSON.parse(response.body, symbolize_names: true)
+    response = Faraday.get "http://api.eventful.com/json/events/search?app_key=8gWGVrJnPdvtqFrq&location=#{zip_code}&sort_order=popularity&page_size=16"
+    parsed = JSON.parse(response.body, symbolize_names: true)[:events][:event]
+
+    @events = Event.get_events(parsed)
+    binding.pry
+  end
+end
+
+class Event
+  attr_reader :title, :start_time, :descripton, :venue_name, :venue_address
+  def initialize(data)
+    @name = data[:title]
+    @time = data[:start_time]
+    @description = data[:descripton]
+    @venue_name = data[:venue_name]
+    @venue_address = data[:venue_address]
+  end
+  def self.get_events(data)
+    data.map do |event|
+      Event.new(event)
+    end
   end
 end
