505fcf8bbda4623d3e5bf6ae16d90951c34cb9b5
Jesse Shipley <jesse.radack@gmail.com>
2017-02-09 11:06:01 -0700

    add basic view to events index

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,18 +6,17 @@ class Event
               :venue_address
 
   def initialize(attrs = {})
-    @name = attrs[]
-    @name = attrs[]
-    @name = attrs[]
-    @name = attrs[]
-    @name = attrs[]
-    byebug
+    @name = attrs["title"]
+    @date_time = attrs["start_time"]
+    @description = attrs["description"]
+    @venue = attrs["venue_name"]
+    @venue_address = attrs["venue_address"]
   end
 
   def self.list(params)
     response = EventService.new.gather_events(params)
     results = Hash.from_trusted_xml(response.body)
-    
+
     results["search"]["events"]["event"].each do |event|
       new(event)
     end
