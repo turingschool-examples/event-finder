b06d6e93044507f48f471157d150f7dfbc8a37f5
Mike Schutte <tmikeschutte@gmail.com>
2017-02-09 10:53:18 -0700

    Adjust capybara selectors for test
    Coerce null descriptions to string
    
    Map service.events return value
    
    Add classed article to view
    
    Add page limit and sort order to request

--- app/services/eventful_service.rb
+++ app/services/eventful_service.rb
@@ -3,8 +3,8 @@ class EventfulService
   end
 
   def events(location)
-    response = Faraday.get("http://api.eventful.com/json/events/search?&location=#{location}&sort_order=popularity&app_key=#{ENV["EVENTFUL_API_KEY"]}&date=march")
+    response = Faraday.get("http://api.eventful.com/json/events/search?&location=#{location}&sort_order=popularity&app_key=#{ENV["EVENTFUL_API_KEY"]}&date=march&sort_direction=descending&page_size=16")
     raw_events = JSON.parse(response.body)["events"]["event"]
-    raw_events.each { |event| Event.new(event) }
+    raw_events.map { |event| Event.new(event) }
   end
 end
--- spec/features/events/visitor_can_search_for_events_by_zipcode_spec.rb
+++ spec/features/events/visitor_can_search_for_events_by_zipcode_spec.rb
@@ -13,20 +13,21 @@ RSpec.describe do
   context "visitor" do
     it "views events for a given zipcode" do
       VCR.use_cassette("events") do
-        Net::HTTP.get_response(URI("http://api.eventful.com/json/events/search?&location=80202&sort_order=popularity&app_key=#{ENV["EVENTFUL_API_KEY"]}&date=march"))
+        Net::HTTP.get_response(URI("http://api.eventful.com/json/events/search?&location=80202&sort_order=popularity&app_key=#{ENV["EVENTFUL_API_KEY"]}&date=march&sort_direction=descending&page_size=16"))
         visit root_path
 
         fill_in :q, with: 80202
         click_on "Find Events"
         expect(current_path).to eq search_path
         expect(page).to have_content "16 Events for March"
-        within first("event") do
-          expect(page).to have_content "Event:"
-          expect(page).to have_content "Date:"
+        expect(page).to have_selector(".event", count: 16)
+        within first(".event") do
+          expect(page).to have_content "Event: Experience Hendrix"
+          expect(page).to have_content "Date: 2017-03-07"
           expect(page).to have_content "Time:"
           expect(page).to have_content "Description:"
           expect(page).to have_content "Venue:"
-          # expect(page).to have_link "venue_name", href: venue_path(venue)
+          expect(page).to have_link "venue_name", href: venue_path(venue)
           expect(page).to have_content "Venue Address:"
         end
       end
