a0896dee589456afea67e93273a51868cb50a3b5
Laszlo Balogh <laszlo@thebaloghs.com>
2017-02-09 10:59:24 -0700

    Refactor EventFinderService, moving Faraday call to its own method. Correct typo in main api url constant name

--- app/models/event_finder_service.rb
+++ app/models/event_finder_service.rb
@@ -1,6 +1,7 @@
 class EventFinderService
-  EVENFINDER_API_URL_JSON = "http://api.eventful.com/json"
+  EVENTFINDER_API_URL_JSON = "http://api.eventful.com/json"
   ENDPOINT_EVENTS_SEARCH = "/events/search"
+  EVENTFINDER_ENDPOINTS = {:search => ENDPOINT_EVENTS_SEARCH}
 
   def initialize(search_params = {})
     @_location = search_params[:location]
@@ -9,10 +10,14 @@ class EventFinderService
   end
 
   def events
-    raw_response = Faraday.get "#{EVENFINDER_API_URL_JSON}#{ENDPOINT_EVENTS_SEARCH}?app_key=7J7CGGFM5zgs8RH7&location=#{@_location}&date=2017030100-2017033100&sort_order=#{@_sort_by}&page_size=#{@_result_limit}&page_number=1"
+    # raw_response = Faraday.get "#{EVENTFINDER_API_URL_JSON}#{ENDPOINT_EVENTS_SEARCH}?app_key=7J7CGGFM5zgs8RH7&location=#{@_location}&date=2017030100-2017033100&sort_order=#{@_sort_by}&page_size=#{@_result_limit}&page_number=1"
+    raw_response = eventfinder_api_call
     response = JSON.parse(raw_response.body)
     response["events"]["event"]
   end
 
+  def eventfinder_api_call
+    Faraday.get "#{EVENTFINDER_API_URL_JSON}#{ENDPOINT_EVENTS_SEARCH}?app_key=7J7CGGFM5zgs8RH7&location=#{@_location}&date=2017030100-2017033100&sort_order=#{@_sort_by}&page_size=#{@_result_limit}&page_number=1"
+  end
 
 end
