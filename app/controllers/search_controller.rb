1e47bfb4e9ed09e306a45c6388f1f41b91e4cf1e
David Knott <david1k1nott@gmail.com>
2017-02-09 10:02:21 -0700

    create event class

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,30 @@
 class SearchController < ApplicationController
   def index
-    raw_response = Faraday.get "http://api.eventful.com/json/events/search?where=80202&app_key=Q5gxTfWsVGgbDX8r&when=march&sort_order=popularity&page_size=16&page_number=1"
+    raw_response = Faraday.get "http://api.eventful.com/json/events/search?where=80202&app_key=Q5gxTfWsVGgbDX8r&when=march&sort_order=popularity"
+    response = JSON.parse(raw_response.body, symbolize_names: true)[:events][:event]
+    @events = response.map do |event|
+      Event.new(event)
+    end
     byebug
-    response = JSON.parse(raw_response.body, symbolize_names: true)
   end
+end
+
+class Event
+
+  attr_reader :name, 
+              :description,
+              :start_time,
+              :venue,
+              :venue_url,
+              :venue_address
+
+  def initialize(event)
+    @name = event[:title]
+    @description = event[:description]
+    @start_time = event[:start_time]
+    @venue = event[:venue_name]
+    @venue_url = event[:venue_url]
+    @venue_address = event[:venue_address]
+  end
+
 end
