c782b3e385df325d17581f206baf710561bd262b
Nicholas Erhardt <ski.climb.europe@gmail.com>
2017-02-09 09:35:23 -0700

    First pass at spec

--- spec/features/guest_can_find_popular_events_spec.rb
+++ spec/features/guest_can_find_popular_events_spec.rb
@@ -9,7 +9,18 @@ require 'rails_helper'
 
 describe "Viewing events" do
   context "given a zipcode" do
+    scenario "user sees a list of events in that zipcode", :vcr => true do
+      visit root_path
+      within: "q", fill_in: "80202"
+      click_on "Find Events"
 
+      expect(page).to have_current_path(search_path)
+      expect(page).to have_content "16 events for March"
+      expect(page).to have_css(".event", count: 16)
+      within first(".event") do
+        expect(page).to have_content "Lil Wayne  - A Boogie Wit Da Hoodie"
+      end
+    end
   end
 
 end
