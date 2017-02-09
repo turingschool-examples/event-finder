21f44e1f5360c77cf292941079a9e9dcf80ff2df
Mike Schutte <tmikeschutte@gmail.com>
2017-02-09 10:25:56 -0700

    Develop eventful service
    Set up Event poro and spec

--- app/services/eventful_service.rb
+++ app/services/eventful_service.rb
@@ -3,6 +3,8 @@ class EventfulService
   end
 
   def events(location)
-
+    response = Faraday.get("http://api.eventful.com/json/events/search?&location=#{location}&sort_order=popularity&app_key=#{ENV["EVENTFUL_API_KEY"]}")
+    raw_events = JSON.parse(response.body)["events"]
+    raw_events.each { |event| Event.create(event) }
   end
 end
