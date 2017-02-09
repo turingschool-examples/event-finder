dc007e83cd69f38c8f3f910e6c88ec15dc00c5a4
Brett Rivers <riverswb@gmail.com>
2017-02-09 10:19:13 -0700

    Failing to use app key

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,15 @@ class SearchController < ApplicationController
   def index
     zip = params[:q]
     date = "March"
-    conn = Faraday.post("http://api.eventful.com/rest/events/search?BKCFLWH22GnWSjtMs&location=zip&") do |faraday|
-      faraday.get
+    conn = Faraday.new(url: "http://api.eventful.com/rest/events/search")
+    response = conn.post do |p|
+      p.headers['app_key'] = "BKCFLWH22GnWSjtMs"
+      p.params[:location] = 80202
     end
+    # faraday[:headers] = :app_key => "BKCFLWH22GnWSjtMs"
+    # response = conn.post #, {:app_key => "BKCFLWH22GnWSjtMs"}
+
+    # key = BKCFLWH22GnWSjtMs
 
 
   end
